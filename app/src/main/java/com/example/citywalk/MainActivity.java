33ea2be805909f4bf11dfb46aaeea4f2490d726e
yitong bian <cucucu737@163.com~>
2023-10-07 19:22:32 +0800

    接入腾讯地图API,实现地图调用。绘制基础的界面选择按钮。

--- app/src/main/java/com/example/citywalk/MainActivity.java
+++ app/src/main/java/com/example/citywalk/MainActivity.java
@@ -1,13 +1,64 @@
 package com.example.citywalk;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.tencent.tencentmap.mapsdk.maps.TencentMap;
+import com.tencent.tencentmap.mapsdk.maps.TencentMapInitializer;
+import com.tencent.tencentmap.mapsdk.maps.TextureMapView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextureMapView mapView;
+    private TencentMap tencentMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TencentMapInitializer.setAgreePrivacy(true);
+
+        mapView = findViewById(R.id.mapView);
+
+        tencentMap = mapView.getMap();
     }
+
+    /**
+     * mapview的生命周期管理
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        mapView.onRestart();
+    }
+
 }
