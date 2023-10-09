26444e1e98491e0d6da191a7af3878f28f06d4d4
xiangyuff <2806087621@qq.com>
2023-10-10 00:51:59 +0800

    mwz modify

--- app/src/main/java/com/example/citywalk/MainActivity.java
+++ app/src/main/java/com/example/citywalk/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.citywalk;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
 import android.widget.ImageButton;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import com.tencent.tencentmap.mapsdk.maps.TencentMap;
 import com.tencent.tencentmap.mapsdk.maps.TencentMapInitializer;
@@ -38,6 +40,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         analyse_btn.setOnClickListener(this);
         setting_btn.setOnClickListener(this);
 
+        //隐藏标题栏
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        //设置顶部状态栏为透明
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
     }
 
     /**
