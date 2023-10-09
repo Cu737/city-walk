9a9dbc75641797c1cfc9483e2cd932cfa47fac39
yitong bian <cucucu737@163.com~>
2023-10-09 17:24:44 +0800

    新增diary界面，实现其与主界面的跳转。

---
+++ app/src/main/java/com/example/citywalk/DiaryActivity.java
@@ -0,0 +1,42 @@
+package com.example.citywalk;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageButton;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DiaryActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary);
+
+        //获取五个按钮
+        ImageButton add_btn = findViewById(R.id.add_btn);
+        ImageButton all_btn = findViewById(R.id.all_btn);
+        ImageButton diary_btn = findViewById(R.id.dairy_btn);
+        ImageButton analyse_btn = findViewById(R.id.analyse_btn);
+        ImageButton setting_btn = findViewById(R.id.setting_btn);
+        //设置监听事件
+        add_btn.setOnClickListener(this);
+        all_btn.setOnClickListener(this);
+        diary_btn.setOnClickListener(this);
+        analyse_btn.setOnClickListener(this);
+        setting_btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int btn_id = view.getId();
+        //转入日记界面
+        if(btn_id == R.id.add_btn)
+        {
+            Intent diary_intent = new Intent(this, MainActivity.class);
+            diary_intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(diary_intent);
+        }
+
+    }
+}
