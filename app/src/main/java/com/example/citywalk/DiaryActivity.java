00dfa2307bb0b9c17cde71ae4f29ca841e9e6884
yitong bian <cucucu737@163.com~>
2023-10-11 11:17:05 +0800

    将关于按钮跳转和获取按钮的几段重复的代码封装。

--- app/src/main/java/com/example/citywalk/DiaryActivity.java
+++ app/src/main/java/com/example/citywalk/DiaryActivity.java
@@ -1,12 +1,12 @@
 package com.example.citywalk;
 
-import android.content.Intent;
+
 import android.graphics.Color;
 import android.view.View;
-import android.widget.ImageButton;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.citywalk.util.ButtonChoose;
 
 public class DiaryActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -15,18 +15,7 @@ public class DiaryActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
 
-        //获取五个按钮
-        ImageButton add_btn = findViewById(R.id.add_btn);
-        ImageButton all_btn = findViewById(R.id.all_btn);
-        ImageButton diary_btn = findViewById(R.id.dairy_btn);
-        ImageButton analyse_btn = findViewById(R.id.analyse_btn);
-        ImageButton setting_btn = findViewById(R.id.setting_btn);
-        //设置监听事件
-        add_btn.setOnClickListener(this);
-        all_btn.setOnClickListener(this);
-        diary_btn.setOnClickListener(this);
-        analyse_btn.setOnClickListener(this);
-        setting_btn.setOnClickListener(this);
+        ButtonChoose.initButton(this);
 
         //隐藏标题栏
         ActionBar actionBar = getSupportActionBar();
@@ -39,14 +28,8 @@ public class DiaryActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View view) {
-        int btn_id = view.getId();
-        //转入日记界面
-        if(btn_id == R.id.add_btn)
-        {
-            Intent diary_intent = new Intent(this, MainActivity.class);
-            diary_intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-            startActivity(diary_intent);
-        }
+        //调用关于各个按钮的监听
+        ButtonChoose.chooseButton(this,view);
 
     }
 }
---
+++ app/src/main/java/com/example/citywalk/util/ButtonChoose.java
@@ -0,0 +1,51 @@
+package com.example.citywalk.util;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageButton;
+import com.example.citywalk.DiaryActivity;
+import com.example.citywalk.MainActivity;
+import com.example.citywalk.R;
+
+public class ButtonChoose {
+    public static void initButton(Activity activity)
+    {
+        //封装设置初始化按钮并设置监听的代码
+        ImageButton add_btn = activity.findViewById(R.id.add_btn);
+        ImageButton all_btn = activity.findViewById(R.id.all_btn);
+        ImageButton diary_btn = activity.findViewById(R.id.dairy_btn);
+        ImageButton analyse_btn = activity.findViewById(R.id.analyse_btn);
+        ImageButton setting_btn = activity.findViewById(R.id.setting_btn);
+
+        add_btn.setOnClickListener((View.OnClickListener) activity);
+        all_btn.setOnClickListener((View.OnClickListener) activity);
+        diary_btn.setOnClickListener((View.OnClickListener) activity);
+        analyse_btn.setOnClickListener((View.OnClickListener) activity);
+        setting_btn.setOnClickListener((View.OnClickListener) activity);
+    }
+    public static void chooseButton(Activity activity,View view)
+    {
+        //封装按钮跳转代码
+        int btn_id = view.getId();
+        //转入日记界面
+        if(btn_id == R.id.add_btn && activity.getLocalClassName().equals("MainActivity"))
+        {
+
+        }
+        else if(btn_id == R.id.add_btn)
+        {
+            Intent diary_intent = new Intent(activity, MainActivity.class);
+            diary_intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            activity.startActivity(diary_intent);
+        }
+        else if (btn_id == R.id.dairy_btn)
+        {
+            Intent diary_intent = new Intent(activity, DiaryActivity.class);
+            diary_intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            activity.startActivity(diary_intent);
+        }
+    }
+
+
+}
