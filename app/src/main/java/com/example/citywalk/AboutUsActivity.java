ecc8d5b6a44b8ae5527d010340cca7321d94887c
yitong bian <cucucu737@163.com~>
2023-11-20 17:13:10 +0800

    将“关于我们”和“使用说明”的顶部状态栏设为透明，并且去除背景的圆角。

--- app/src/main/java/com/example/citywalk/AboutUsActivity.java
+++ app/src/main/java/com/example/citywalk/AboutUsActivity.java
@@ -1,5 +1,7 @@
 package com.example.citywalk;
 
+import android.graphics.Color;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,10 @@ public class AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        //设置顶部状态栏为透明
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
     }
+
 }
