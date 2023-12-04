e1a3179bf44d62a00d99668c254029d1a05c63b3
xiangyuff <2806087621@qq.com>
2023-12-04 16:33:31 +0800

    完成日记展示前端

--- app/src/main/java/com/example/citywalk/DiaryActivity.java
+++ app/src/main/java/com/example/citywalk/DiaryActivity.java
@@ -2,13 +2,21 @@ package com.example.citywalk;
 
 
 import android.graphics.Color;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.citywalk.util.ButtonChoose;
 
+
+
+
 public class DiaryActivity extends AppCompatActivity implements View.OnClickListener{
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,28 @@ public class DiaryActivity extends AppCompatActivity implements View.OnClickList
 
         ButtonChoose.initButton(this);
 
+        LinearLayout item_1 = (LinearLayout)this.findViewById(R.id.item_1);
+
+        LinearLayout bigbox = (LinearLayout)this.findViewById(R.id.big_box);
+        ImageView cancel_bn=(ImageView)this.findViewById(R.id.bigbox_cancel);
+
+
+
+        item_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bigbox.setVisibility(View.VISIBLE);
+            }
+        });
+
+        cancel_bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bigbox.setVisibility(View.INVISIBLE);
+            }
+        });
+
+
         //隐藏标题栏
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
@@ -24,7 +54,9 @@ public class DiaryActivity extends AppCompatActivity implements View.OnClickList
         //设置顶部状态栏为透明
         getWindow().setStatusBarColor(Color.TRANSPARENT);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+
     }
+
 
     @Override
     public void onClick(View view) {
